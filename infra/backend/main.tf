a943b0982b52a84fc86fcefce5387e50ea2dd488
Aswin1118 <aswinvj390@gmail.com>
2023-08-12 14:21:02 +0530

    Added variables to s3 and dynamodb

--- infra/backend/main.tf
+++ infra/backend/main.tf
@@ -1,8 +1,17 @@
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
 # Declare the state lock module
 module "backend" {
-  source                = "../../modules/backend"
+  source            = "../../modules/backend"
+  region            = var.region
+  billing_mode      = var.billing_mode
+  hash_key          = var.hash_key
+  attribute_name    = var.attribute_name
+  attribute_type    = var.attribute_type
+  owner             = var.owner
+  environment       = var.environment
+  cost_center       = var.cost_center
+  application       = var.application
 }
--- modules/backend/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name        = "Terraform State Bucket"
-    Environment = "Dev"
-  }
-}
