8d29ac2f763b240b244f1357826e25be4a92da20
Aswin1118 <aswinvj390@gmail.com>
2023-10-04 15:32:16 +0530

    [TEC-56][Add] - VPC endpoints and NACL

--- modules/vpc/route-tables.tf
+++ modules/vpc/route-tables.tf
@@ -82,3 +82,24 @@ resource "aws_route" "management" {
   destination_cidr_block = var.destination_cidr_block
   gateway_id             = aws_internet_gateway.main.id
 }
+
+resource "aws_route_table" "platform" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    {
+      Name        = "${var.environment}-${var.application}-platform-route-table",
+      Environment = var.environment,
+      Owner       = var.owner,
+      CostCenter  = var.cost_center,
+      Application = var.application
+    },
+    var.tags
+  )
+}
+
+resource "aws_route" "platform" {
+  route_table_id         = aws_route_table.platform.id
+  destination_cidr_block = var.destination_cidr_block
+  nat_gateway_id         = aws_internet_gateway.main.id
+}
